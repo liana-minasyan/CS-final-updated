cef23c94746b279f7f5fcb268cfae39658f93f5a
lianamay <liana_minasyan@edu.aua.am>
2016-12-15 01:09:34 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,28 @@
 var ball;
 var plate;
 var breaks = [];
-var y = 50;
+var y = 70;
 var bg;
+var t = [];
+
 
 function setup() {
- var mycanvas = createCanvas(600,600);
-  bg = loadImage("https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Fornax_Dwarf.jpg/600px-Fornax_Dwarf.jpg");
+ bg = loadImage("https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Fornax_Dwarf.jpg/600px-Fornax_Dwarf.jpg");
+ canvas = createCanvas(600,600);
+  
   ball = new Ball ();
   
   plate = new Plate ();
-
+ 
   for(i = 0; i < 15; i++ ){
     breaks[i] = new Breaks(55+i*120, y);
     if (breaks[i].x + breaks[i].length > width){
-      breaks[i].y = breaks[i].y + 50 * floor((breaks[i].x + breaks[i].length/2)/width);
+      breaks[i].y = breaks[i].y + 70 * floor((breaks[i].x + breaks[i].length/2)/width);
       breaks[i].x = breaks[i].x - width * floor((breaks[i].x + breaks[i].length/2)/width);
     }
   }
 }
+
 
 function draw() {
   background(bg);
@@ -27,7 +31,23 @@ function draw() {
   ball.bounce();
   plate.draw();
   plate.move();
- 
+
+//score 
+  for(var j = 0; j < 15; j++){
+    t[j] = 100*(j+1) + "";
+      textSize(16);
+      fill(95,255,23);
+      if(ball.y < 70){
+      text (t[0],520,20);
+    }
+  }
+ textSize(16);
+ fill(95,255,23);
+ text("Score", 450, 20);
+
+
+
+
   //ball.removing();
   fill(75, 127, 232);
   for(i = 0; i < breaks.length; i++){
@@ -51,5 +71,7 @@ function keyReleased (){
 function keyIsPressed() {
   
 }*/
+
+
 
 
