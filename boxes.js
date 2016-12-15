f1d6cf19c41c38e480e8a125fc6156a744101f0b
lianamay <liana_minasyan@edu.aua.am>
2016-12-15 22:31:34 +0400

    Update boxes.js

--- boxes.js
+++ boxes.js
@@ -1,13 +1,13 @@
-function Breaks(x,y) {
+function Breaks(x,y,z) {
  
   this.x = x;
   this.y = y;
   
-  this.length = 70;
+  this.length = z;
   
   this.height = 30;
   
   this.createBreaks = function () {
 	rect(this.x,this.y,this.length,this.height);
   }
-}
+} 
