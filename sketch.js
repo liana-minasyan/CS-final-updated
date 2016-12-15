386dbfca756954548fd7a5365bb9712e8fa56e7c
lianamay <liana_minasyan@edu.aua.am>
2016-12-15 22:36:53 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,19 +3,41 @@ var plate;
 var breaks = [];
 var y = 70;
 var bg;
-var t = [];
-
+var t = 0;
+var player;
+var lifes = [];
+var number = 3;
+var rbutton;
 
 function setup() {
  bg = loadImage("https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Fornax_Dwarf.jpg/600px-Fornax_Dwarf.jpg");
  canvas = createCanvas(600,600);
-  
-  ball = new Ball ();
+  Recetpage();
+  ball.xspeed = 0;
+  ball.yspeed = 0;
+  var posit = canvas.width/2 + innerWidth/2;
+  var button = createButton("Click here to Start");
+  button.mousePressed(Recetpage);
+
+  button.position(posit,200);
+
+ rbutton = createButton("Restart",680,300);
+ rbutton.mousePressed(Recover);
+ rbutton.position(posit,230);
+}
+
+function Recetpage(){
+   ball = new Ball ();
   
   plate = new Plate ();
- 
-  for(i = 0; i < 15; i++ ){
-    breaks[i] = new Breaks(55+i*120, y);
+
+  player = new Game ();
+
+
+ t = 0;
+ number = 3;
+  for(i = 0; i < 25; i++ ){
+    breaks[i] = new Breaks(55+i*120, y,50);
     if (breaks[i].x + breaks[i].length > width){
       breaks[i].y = breaks[i].y + 70 * floor((breaks[i].x + breaks[i].length/2)/width);
       breaks[i].x = breaks[i].x - width * floor((breaks[i].x + breaks[i].length/2)/width);
@@ -23,35 +45,22 @@ function setup() {
   }
 }
 
-
 function draw() {
   background(bg);
+  player.lifes();
+  player.gameOver();
+  player.score();
+  player.winGame();
   ball.display();
   ball.move();
   ball.bounce();
   plate.draw();
   plate.move();
-
-//score 
-  for(var j = 0; j < 15; j++){
-    t[j] = 100*(j+1) + "";
-      textSize(16);
-      fill(95,255,23);
-      if(ball.y < 70){
-      text (t[0],520,20);
-    }
-  }
- textSize(16);
- fill(95,255,23);
- text("Score", 450, 20);
-
-
-
-
-  //ball.removing();
-  fill(75, 127, 232);
+  
+    fill(75, 127, 232);
   for(i = 0; i < breaks.length; i++){
    breaks[i].createBreaks();
+   ball.distroy();
   }
   
   ball.changeDir();
@@ -67,10 +76,23 @@ function draw() {
 function keyReleased (){
   plate.dir(0);
 }
-/*
-function keyIsPressed() {
-  
+
+/*function keyIsPressed() {
+  loop()
 }*/
+function Recover(){
+  loop();
+
+Recetpage();
+ball.xspeed = 0;
+ball.yspeed = 0;
+}
+
+
+
+
+
+
 
 
 
